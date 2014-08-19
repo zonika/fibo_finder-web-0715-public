424c3d2da76d7b6509dae49205b9930017157ac2
Arel English <arelenglish@gmail.com>
2014-08-19 09:57:31 -0400

    updating rspec

--- spec/fibo_finder_spec.rb
+++ spec/fibo_finder_spec.rb
@@ -2,23 +2,23 @@ require 'spec_helper'
 
 describe "fibo finder" do
 
-  it "it should return the nth number of the fibonacci sequence" do
+  it "returns the nth number of the fibonacci sequence" do
     expect(fibo_finder(0)).to eq(0)
   end
 
-  it "it should return the nth number of the fibonacci sequence" do
+  it "returns the nth number of the fibonacci sequence" do
     expect(fibo_finder(1)).to eq(1)
   end
 
-  it "it should return the nth number of the fibonacci sequence" do
+  it "returns the nth number of the fibonacci sequence" do
     expect(fibo_finder(4)).to eq(3)
   end
 
-  it "it should return the nth number of the fibonacci sequence" do
+  it "returns the nth number of the fibonacci sequence" do
     expect(fibo_finder(7)).to eq(13)
   end
 
-  it "it should return the nth number of the fibonacci sequence" do
+  it "returns the nth number of the fibonacci sequence" do
     expect(fibo_finder(10)).to eq(55)
   end
 
